CruelDrool/GuildRosterExport

commit 90237ba65a2e09b8d1b350e1d483137140e96bc8

Fix sorting using columns with boolean values.

Namespaces/Core.lua
@@ -316,15 +316,15 @@ function Core:ExportData(fileFormat, saveToDB)
 			local secondarySortOrder = Private.db.profile.sorting.secondarySortOrder
 
 			table.sort(fullRoster, function(a, b)
-				if a[primaryColumnID] > b[primaryColumnID] then
+				if tostring(a[primaryColumnID]) > tostring(b[primaryColumnID]) then
 					return primarySortOrder == 2
-				elseif a[primaryColumnID] < b[primaryColumnID] then
+				elseif tostring(a[primaryColumnID]) < tostring(b[primaryColumnID]) then
 					return primarySortOrder == 1
 				else
 					if secondarySortOrder == 1 then
-						return a[secondaryColumnID] < b[secondaryColumnID]
+						return tostring(a[secondaryColumnID]) < tostring(b[secondaryColumnID])
 					else
-						return a[secondaryColumnID] > b[secondaryColumnID]
+						return tostring(a[secondaryColumnID]) > tostring(b[secondaryColumnID])
 					end
 				end
 			end)

commit 749f42f8286ed9fd8e56149fc2804d87b674bf19

Fix sorting bug introduced in 90237ba
Was converting numbers to string. D'oh!

Namespaces/Core.lua
@@ -316,15 +316,20 @@ function Core:ExportData(fileFormat, saveToDB)
 			local secondarySortOrder = Private.db.profile.sorting.secondarySortOrder
 
 			table.sort(fullRoster, function(a, b)
-				if tostring(a[primaryColumnID]) > tostring(b[primaryColumnID]) then
+				local A = type(a[primaryColumnID]) == "boolean" and tostring(a[primaryColumnID]) or a[primaryColumnID]
+				local B = type(b[primaryColumnID]) == "boolean" and tostring(b[primaryColumnID]) or b[primaryColumnID]
+
+				if A > B then
 					return primarySortOrder == 2
-				elseif tostring(a[primaryColumnID]) < tostring(b[primaryColumnID]) then
+				elseif A < B then
 					return primarySortOrder == 1
 				else
+					A = type(a[secondaryColumnID]) == "boolean" and tostring(a[secondaryColumnID]) or a[secondaryColumnID]
+					B = type(b[secondaryColumnID]) == "boolean" and tostring(b[secondaryColumnID]) or b[secondaryColumnID]
 					if secondarySortOrder == 1 then
-						return tostring(a[secondaryColumnID]) < tostring(b[secondaryColumnID])
+						return A < B
 					else
-						return tostring(a[secondaryColumnID]) > tostring(b[secondaryColumnID])
+						return A > B
 					end
 				end
 			end)